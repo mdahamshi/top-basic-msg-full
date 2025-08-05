614015a2625e5496fdd02df1fb30811b61ae46ca
Mohammad Dahamshi <mohammad.dahamshi@gmail.com>
2025-08-05 12:25:13 +0300

    adds rate limit

--- server/expressInit.js
+++ server/expressInit.js
@@ -2,6 +2,18 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+
+const messageLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again later.'
+});
+const globalLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 1000,
+  message: 'Too many requests overall. Please try again later.'
+});
 
 function initExpressApp(app) {
   // Locals
@@ -24,6 +36,10 @@ function initExpressApp(app) {
   // Static files
   const clientBuildPath = path.resolve(__dirname, '../client/dist');
   app.use(express.static(clientBuildPath));
+  // rate limit
+  app.use('/api/messages', messageLimiter);
+  app.use(globalLimiter);
+
 }
 
 module.exports = initExpressApp;
